85d6fecb8542d5367455da2a5402bce48e3458e0
Nikolas <nikolasc@apgsensors.com>
2025-08-18 10:26:25 -0600

    added repl target to allow for basic interactive testing. added ostream overload for action codes

--- include/dice_parser/action_code.hpp
+++ include/dice_parser/action_code.hpp
@@ -1,6 +1,8 @@
 #ifndef ACTION_CODE_HPP_GUARD
 #define ACTION_CODE_HPP_GUARD
 
+#include <ostream>
+
 enum class action_code
 {
     action_success,
@@ -8,5 +10,25 @@ enum class action_code
     invalid_syntax,
     variable_error,
 };
+
+inline std::ostream& operator<<(std::ostream& os, action_code& code)
+{
+    switch(code)
+    {
+    case action_code::action_success:
+        os << "action_code::action_success";
+        break;
+    case action_code::unknown_symbol:
+        os << "action_code::unknown_symbol";
+        break;
+    case action_code::invalid_syntax:
+        os << "action_code::invalid_syntax";
+        break;
+    case action_code::variable_error:
+        os << "action_code::variable_error";
+        break;
+    }
+    return os;
+}
 
 #endif//ACTION_CODE_HPP_GUARD
---
+++ test/repl.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include "dice_parser/dice_parser.hpp"
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "****************************************************************" << std::endl;
+    std::cout << "*   ___  _          ___                       ___          _   *" << std::endl;
+    std::cout << "*  |   \\(_)__ ___  | _ \\__ _ _ _ ______ _ _  | _ \\___ _ __| |  *" << std::endl;
+    std::cout << "*  | |) | / _/ -_) |  _/ _` | '_(_-/ -_| '_| |   / -_| '_ | |  *" << std::endl;
+    std::cout << "*  |___/|_\\__\\___| |_| \\__,_|_| /__\\___|_|   |_|_\\___| .__|_|  *" << std::endl;
+    std::cout << "*                                                    |_|       *" << std::endl;
+    std::cout << "*  enter  \"quit \" to exit program                              *" << std::endl;
+    std::cout << "****************************************************************" << std::endl;
+
+    DiceParser parser;
+    std::string user_input;
+    while(user_input != "quit")
+    {
+        std::cout << ">> ";
+        std::cin >> user_input;
+        if(user_input != "quit")
+        {
+            auto result = parser.parse(user_input);
+            if(std::holds_alternative<double>(result))
+                std::cout << ">> " << std::get<double>(result) <<std::endl;
+            else if(std::holds_alternative<DiceDistr>(result))
+                std::cout << ">> " << std::get<DiceDistr>(result).get_expr() <<std::endl;
+            else if(std::holds_alternative<action_code>(result))
+                std::cout << ">> " << std::get<action_code>(result) <<std::endl;
+            else
+                std::cout << ">> " << "unknown return type" << std::endl;
+        }
+    }
+}
